2f00908e4fc02be1adbbe3e1b3ea0e01cc0bf41d
nicolas <nicolas.castel@outlook.com>
2021-03-16 15:11:17 +0100

    msd working

--- msd.py
+++ msd.py
@@ -5,8 +5,9 @@ Module containing msd related methods
 import ase
 import ase.data
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 import sadi.trajectory
 import sadi.atom
@@ -21,17 +22,17 @@ class Msd(object):
         self.msd_data = []
 
     @classmethod
-    def from_trajectory(cls, trajectory, dump_freq = 1):
+    def from_trajectory(cls, trajectory, delta_Step = 1):
         """
         constructor of msd class from an ase trajectory object
 
         """
         msd_class = cls() # initialize class
-        msd_class.compute_msd(trajectory, dr, rmax)
+        msd_class.compute_msd(trajectory, delta_Step)
         return msd_class # return class as it is a constructor
 
     @staticmethod
-    def compute_species_msd(self, trajectory, atomic_number = None):
+    def compute_species_msd(trajectory, atomic_number = None):
         """calculate MSD with real pos (stored in r) compared to PBC pos stored
         in ase (extracted with position)
         if atomic_number is None, compute MSD between all atoms
@@ -54,15 +55,15 @@ class Msd(object):
             MSD[t] = np.linalg.norm(r[t]-r_0)**2/len(r_0)
         return MSD
 
-    def compute_msd(self, trajectory, dump_freq):
+    def compute_msd(self, trajectory, delta_Step):
         """
         Args:
             trajectory: ase trajectory object
-            dump_freq: number of simulation steps between two frames
+            delta_Step: number of simulation steps between two frames
         """
         elements = sadi.atom.get_atomic_numbers_unique(trajectory[0])
 
-        Step = np.arange(len(trajectory)) * dump_freq        
+        Step = np.arange(len(trajectory)) * delta_Step        
         self.msd_data = pd.DataFrame({"Step": Step})
         self.msd_data["X"] = Msd.compute_species_msd(trajectory)
 
@@ -72,19 +73,19 @@ class Msd(object):
 
     def write_to_file(self, path_to_output):
         """path_to_output: where the MSD object will be written"""
-        self.msd_data.to_feather(path_to_output + ".rdf")
+        self.msd_data.to_feather(path_to_output + ".msd")
 
     @classmethod
     def from_msd(cls, path_to_msd):
         """
-        constructor of rdf class from rdf file
+        constructor of msd class from msd file
         """
         msd_class = cls() # initialize class
         msd_class.read_msd_file(path_to_msd)
         return msd_class # return class as it is a constructor
 
     def read_msd_file(self, path_to_data, add_file_extension = True):
-        """add_file_extension: if True will try to add .rdf if not already present"""
-        if add_file_extension and path_to_data[-4:] != ".rdf":
-            path_to_data += ".rdf"
-        self.rdf_data = pd.read_feather(path_to_data)
+        """add_file_extension: if True will try to add .msd if not already present"""
+        if add_file_extension and path_to_data[-4:] != ".msd":
+            path_to_data += ".msd"
+        self.msd_data = pd.read_feather(path_to_data)
