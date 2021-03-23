7a9c245a7a4cb25be9d2ed08a0e58191ef18ff19
nicolas <nicolas.castel@outlook.com>
2021-03-23 10:44:48 +0100

    append suffix

--- files/path.py
+++ files/path.py
@@ -10,11 +10,13 @@ def append_suffix(path, suffix):
 
     Args:
         path: pathlib.Path object or string
-        suffix: string object, no initial '.'
+        suffix: string object, if no initial '.' it is added
     Returns:
         path: pathlib.Path object
     """
+    if len(suffix)!=0 and suffix[0]!='.':
+        suffix = '.' + suffix
     path = pathlib.Path(path)
     if path.suffix != suffix:
-        path = path.parent / (path.name + 'suffix')
+        path = path.parent / (path.name + suffix)
     return path
