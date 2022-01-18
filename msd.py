08393e9899234fb199e9bd409489c5f433bed44d
nicolas <nicolas.castel@chimieparistech.psl.eu>
2022-01-18 19:16:37 +0100

    windowmsd, first pass

--- msd.py
+++ msd.py
@@ -21,10 +21,6 @@ class Msd(object):
     Main class for MSD
     """
 
-    def __init__(self):
-        """default constructor"""
-        self.msd_data = pd.DataFrame({"Step": np.empty([0])})
-
     def write_to_file(self, path_to_output):
         """path_to_output: where the MSD object will be written"""
         path_to_output = sadi.files.path.append_suffix(path_to_output, 'msd')
@@ -55,6 +51,10 @@ class DirectMsd(Msd):
 
     MSD(t) = \dfrac{1}{N_{particles}} \sum_{i=1}^{N_{particles}} (r_i(t) - r_i(0))^2 
     """
+
+    def __init__(self):
+        """default constructor"""
+        self.msd_data = pd.DataFrame({"Step": np.empty([0])})
 
     @classmethod
     def from_trajectory(cls, trajectory, delta_Step = 1, first_frame = 0, parallel = False):
@@ -137,78 +137,106 @@ class WindowMsd(Msd):
     Window MSD
 
     MSD(m) = \frac{1}{N_{particles}} \sum_{i=1}^{N_{particles}} \frac{1}{N-m} \sum_{k=0}^{N-m-1} (\vec{r}_i(k+m) - \vec{r}_i(k))^2
+    m is the window length size, usually replaced by time t in publications. 
+    To be unambiguous, here time will carry units and m and k will be used as trajectory indexes
+    The final data array will be y = MSD, x = m labelled 'Time' 
     """
 
+    def __init__(self):
+        """default constructor"""
+        self.msd_data = pd.DataFrame({"Time": np.empty([0])})
+
     @classmethod
-    def from_trajectory(cls, trajectory, delta_Step = 1, first_frame = 0, parallel = False):
+    def from_trajectory(cls, trajectory, delta_time = 100, max_time = "half", timestep = 1,
+             delta_Step = 1, first_frame = 0, parallel = False):
         """
         constructor of msd class
 
         Args:
             trajectory: ase trajectory object
+            delta_time: int, time between two values, in fs
+            max_time: int or str
+                if half, then the upper limit is half of the simulation size
+            timestep: float, timestep between two frames, in fs
             delta_Step: number of simulation steps between two frames
             parallel: Boolean or int (number of cores to use): whether to parallelize the computation
         """
         msd_class = cls() # initialize class
         step = sadi.trajectory.construct_step(delta_Step=delta_Step, first_frame = first_frame, number_of_frames = len(trajectory))
-        msd_class.compute_msd(trajectory, step, parallel)
+        if max_time == "half":
+            max_m = (len(trajectory) // 2)
+            delta_m = delta_time // timestep
+        window = np.arange(0, (max_m // delta_m), delta_m)
+        time = timestep * window
+        msd_class.compute_msd(trajectory, step, window, time, parallel)
         return msd_class # return class as it is a constructor
 
     @staticmethod
-    def compute_species_msd(trajectory, atomic_number = None):
-        """calculate MSD with real pos (stored in r) compared to PBC pos stored
+    def compute_species_msd(trajectory, m, atomic_number = None):
+        """calculate MSD(m) for a given m with real pos (stored in r) compared to PBC pos stored
         in ase (extracted with position)
-        if atomic_number is None, compute MSD between all atoms
+
+        Args:
+            m: int
+            if atomic_number is None, compute MSD between all atoms
         """
-        r_0 = sadi.atom.select_species_positions(trajectory[0], atomic_number)
+        r_k_minus_m = sadi.atom.select_species_positions(trajectory[0], atomic_number)
         # test reducing mem usage
         # r = np.zeros((len(trajectory), len(r_0), 3))
         # r[0] = r_0 
-        r_t = r_0
-        MSD = np.zeros(len(trajectory))
-        for t in range(1, len(trajectory)):
-            r_t_minus_dt = r_t
-            dr = np.zeros((len(r_0), 3))
-            for j in range(3): #x,y,z
-                a = trajectory[t].get_cell()[j,j]
-                # dr[:,j] = (sadi.atom.select_species_positions(trajectory[t], atomic_number) - r[t-1]%a)[:,j]
-                dr[:,j] = (sadi.atom.select_species_positions(trajectory[t], atomic_number) - r_t_minus_dt%a)[:,j]
-                for i in range(len(dr)):
-                    if dr[i][j]>a/2:
-                        dr[i][j] -= a
-                    elif dr[i][j]<-a/2:
-                        dr[i][j] += a
+        r_k = sadi.atom.select_species_positions(trajectory[m], atomic_number)
+        MSD_partial = np.zeros(len(trajectory) - m)
+        for k in range(m, len(trajectory)):
+            r_k_minus_1 = r_k
+            r_k_minus_m_minus_1 = r_k_minus_m
+            def get_dr(k, r_k_minus_1):
+                dr = np.zeros((len(r_k_minus_1), 3))
+                for j in range(3): #x,y,z
+                    a = trajectory[k].get_cell()[j,j]
+                    dr[:,j] = (sadi.atom.select_species_positions(trajectory[k], atomic_number) - r_k_minus_1%a)[:,j]
+                    for i in range(len(dr)):
+                        if dr[i][j]>a/2:
+                            dr[i][j] -= a
+                        elif dr[i][j]<-a/2:
+                            dr[i][j] += a
+            dr_k = get_dr(k, r_k_minus_1)
+            dr_k_minus_m = get_dr(k - m, r_k_minus_m_minus_1)
+            delta_dr = dr_k - dr_k_minus_m
             # r[t] = dr + r[t-1]
             # MSD[t] = np.linalg.norm(r[t]-r_0)**2/len(r_0)
-            r_t = dr + r_t_minus_dt
-            MSD[t] = np.linalg.norm(r_t-r_0)**2/len(r_0)
+            r_k = delta_dr + r_k_minus_1
+            r_k_minus_m = delta_dr + r_k_minus_m_minus_1
+            MSD_partial[k - m] = np.linalg.norm(delta_dr)**2/len(r_k_minus_m)
+        MSD = np.mean(MSD_partial)
         return MSD
 
-    def compute_msd(self, trajectory, step, parallel):
+    def compute_msd(self, trajectory, step, window, time, parallel):
         """
         Args:
             trajectory: ase trajectory object
             step: np array, simulation steps
+            m: np array, window
             parallel: Boolean or int (number of cores to use): whether to parallelize the computation
         """
         logger.info("Start computing msd for %s frames", len(trajectory))
         
         elements = sadi.atom.get_atomic_numbers_unique(trajectory[0])
 
-        Step = step     
-        self.msd_data = pd.DataFrame({"Step": Step})
+        self.msd_data = pd.DataFrame({"Time": m}) 
         if not parallel:
-            self.msd_data["X"] = self.compute_species_msd(trajectory)
+            self.msd_data["X"] = [self.compute_species_msd(trajectory, m) for m in window]
 
             for x in elements:
                 x_str = ase.data.chemical_symbols[x]
-                self.msd_data[x_str] = self.compute_species_msd(trajectory, x)
+                self.msd_data[x_str] = [self.compute_species_msd(trajectory, m, x) for m in window]
         else:
             x_list = [None] + elements
             num_cores = len(x_list) # default value
             if type(parallel) == int and parallel < num_cores:
                 num_cores = parallel
-            msd_list = joblib.Parallel(n_jobs=num_cores)(joblib.delayed(self.compute_species_msd)(trajectory, x) for x in x_list)
+            def compute_for_every_m(trajectory, x):
+                return [self.compute_species_msd(trajectory, x, m) for m in window]
+            msd_list = joblib.Parallel(n_jobs=num_cores)(joblib.delayed(compute_for_every_m)(trajectory, x) for x in x_list)
             self.msd_data["X"] = msd_list[0]
             for i in range(1, len(x_list)):
                 x_str = ase.data.chemical_symbols[x_list[i]]
