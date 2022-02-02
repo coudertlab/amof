3ffa0fd110b54f444967b7b2020f38cdb4334bc6
nicolas <nicolas.castel@chimieparistech.psl.eu>
2022-02-02 19:17:06 +0100

    id os var that works among list of 5

--- msd.py
+++ msd.py
@@ -5,11 +5,11 @@ Module containing msd related methods
 import os
 
 # force numpy to use one thread 
-os.environ["OMP_NUM_THREADS"] = "1"  # essential
-os.environ["OPENBLAS_NUM_THREADS"] = "1"  # at least one of these 4 is needed
-os.environ["MKL_NUM_THREADS"] = "1"  
-os.environ["NUMEXPR_NUM_THREADS"] = "1"  
-os.environ["OPENBLAS_MAIN_FREE"] = "1"
+# os.environ["OMP_NUM_THREADS"] = "1"  
+# os.environ["OPENBLAS_NUM_THREADS"] = "1"   
+# os.environ["MKL_NUM_THREADS"] = "1"  
+os.environ["NUMEXPR_NUM_THREADS"] = "1"  # seems to suffice
+# os.environ["OPENBLAS_MAIN_FREE"] = "1"
 
 import ase
 import ase.data
